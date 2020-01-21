8ba73ee0f94a382425ef0bef52ea87fe2a61fb73
mike <thecooltool@hotmail.com>
2020-01-21 11:28:15 -0500

    Moved MSI build to folder Build.

--- Terra/My Project/AssemblyInfo.vb
+++ Terra/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.0.57")> 
-<Assembly: AssemblyFileVersion("0.1.0.57")> 
+<Assembly: AssemblyVersion("0.1.0.58")> 
+<Assembly: AssemblyFileVersion("0.1.0.58")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
